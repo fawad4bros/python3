b8fba09732fdf13e22f4d5a59f9e47803f877c5f
Fawad naeem <fawad4bros@gmail.com>
2021-02-27 05:40:45 +0500

    Create maps.py

---
+++ maps.py
@@ -0,0 +1,18 @@
+from random import shuffle
+def jumble(word):
+    anagram = list(word)
+    shuffle(anagram)
+    return ''.join(anagram)
+words = ['fawad','hammad','saad','jawad']
+anagrams = []
+
+for word in words:
+    anagrams.append(jumble(word))
+print(anagrams)
+# or
+#map(function,data)
+
+print(list(map(jumble,words)))
+
+#Comprehensions Method
+print([jumble(word) for word in words])
